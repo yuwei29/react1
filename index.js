37ddfc5c3056b063d82e46ef64238b9a8b0b79e2
yuwei29 <121273951+yuwei29@users.noreply.github.com>
2023-02-03 12:21:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import React from 'https://cdn.jsdelivr.net/npm/react@18.2.0/+esm'
-import ReactDOM from 'https://cdn.jsdelivr.net/npm/react-dom@18.2.0/+esm'
-
 function App() {
   return (
     <div className="App">
